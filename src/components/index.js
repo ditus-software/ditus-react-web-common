5389241bf1cab675a935e93c287c56c1297915ad
Andrew McKendrick <ditus@rocketmail.com>
2022-02-07 12:08:31 -0500

    Adds components

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 export { default as Button } from './button';
 export { default as CustomLink } from './custom-link';
+export { default as KeywordSearchBar } from './keyword-search-bar';
 export { default as TextField } from './text-field';
 export { default as TransparentButton } from './transparent-button';
---
+++ src/components/keyword-search-bar/index.js
@@ -0,0 +1,1 @@
+export { default } from './keyword-search-bar';
