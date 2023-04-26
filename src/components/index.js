762c35ce19f297e2459630438674768b9350618d
Andrew McKendrick <ditus@rocketmail.com>
2023-04-26 18:14:54 -0400

    Removes image-magnify component - Too many issues, repo not maintained

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 export { default as Button } from './button';
 export { default as CustomLink } from './custom-link';
-export { default as ImageMagnify } from './image-magnify';
 export { default as KeywordSearchBar } from './keyword-search-bar';
 export { default as Logo } from './logo';
 export { default as Markdown } from './markdown';
