6f468ac99ea84b94882e95aa31d6592a45242cb9
Andrew McKendrick <ditus@rocketmail.com>
2023-04-28 11:37:04 -0400

    Removes two components - They were moved to another NPM package

--- src/components/index.js
+++ src/components/index.js
@@ -5,8 +5,6 @@ export { default as KeywordSearchBar } from './keyword-search-bar';
 export { default as Logo } from './logo';
 export { default as Markdown } from './markdown';
 export { default as MarkdownField } from './markdown-field';
-export { default as RatingField } from './rating-field';
-export { default as TextField } from './text-field';
 export { default as TransparentButton } from './transparent-button';
 export { default as UnboundSelectField } from './unbound-select-field';
 export { default as Vote } from './vote';
